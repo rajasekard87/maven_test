9e6b1ee0b7f48ccb591a298cec83767c5d13de46
rajasekard87 <100590877+rajasekard87@users.noreply.github.com>
2023-05-09 11:43:15 +0530

    Update CalculatorService.java

--- src/main/java/com/mkyong/examples/CalculatorService.java
+++ src/main/java/com/mkyong/examples/CalculatorService.java
@@ -5,7 +5,7 @@ public class CalculatorService {
     public boolean isPositive(int number) {
 
         boolean result = false;
-        if (number >= 0) {
+        if (number < 0) {
             result = true;
         }
         return result;
